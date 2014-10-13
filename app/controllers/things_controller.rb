f87079e4da6c5274cb57c61d3fb3b2a2927f572b
Kenny Johnston <kjohnston.ca@gmail.com>
2014-10-12 22:44:49 -0700

    Add parent editing and pending upload display.

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,5 +1,7 @@
 class ThingsController < ApplicationController
 
+  before_action :find_thing, only: [:show, :edit, :update]
+
   def index
     @things = Thing.all.order("created_at DESC")
   end
@@ -11,7 +13,6 @@ class ThingsController < ApplicationController
 
   def create
     @thing = Thing.new(thing_params)
-    # @thing.new_photo_uploads_uuids = params[:thing][:new_photo_uploads_uuids]
 
     if @thing.save
       redirect_to thing_path(@thing)
@@ -21,10 +22,26 @@ class ThingsController < ApplicationController
   end
 
   def show
-    @thing = Thing.find(params[:id])
+  end
+
+  def edit
+    render "form"
+  end
+
+  def update
+    if @thing.update(thing_params)
+      redirect_to thing_path(@thing)
+    else
+      render "form"
+    end
   end
 
   private
+
+  def find_thing
+    @thing = Thing.find(params[:id])
+    @pending_uploads = @thing.photo_uploads.pending
+  end
 
   def thing_params
     params.require(:thing).permit(:name, new_photo_uploads_uuids: [])
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   mount S3Relay::Engine => "/s3_relay"
 
-  resources :things, only: [:index, :new, :create, :show]
+  resources :things, only: [:index, :new, :create, :show, :edit, :update]
 
   root "things#index"
 
