4658d97c6f1e96e5c72b6be803c27c8ee795cd71
Kenny Johnston <kjohnston.ca@gmail.com>
2014-10-13 18:16:33 -0700

    Update migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141010213242) do
+ActiveRecord::Schema.define(version: 20141014011258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20141010213242) do
     t.integer  "parent_id"
     t.binary   "uuid"
     t.text     "filename"
-    t.text     "public_url"
+    t.string   "content_type"
     t.string   "state"
     t.json     "data"
     t.datetime "uploaded_at"
